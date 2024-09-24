693c0f4dded040ba2ee17077afcc1da79599dc9a
Nirav <niravpatelp129@gmail.com>
2024-09-24 16:52:41 -0400

    * feat(package.json): updated version to '1.0.2' * chore(notarize.js): added console logs for notarization process * chore(signing-script.js): added Windows code signing script

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -3,6 +3,7 @@ const { build } = require('../package.json');
 require('dotenv').config();
 
 exports.default = async function notarizing(context) {
+  console.log('Notarizing...');
   const { electronPlatformName, appOutDir } = context;
   if (electronPlatformName !== 'darwin') {
     return;
@@ -11,14 +12,21 @@ exports.default = async function notarizing(context) {
   const appName = context.packager.appInfo.productFilename;
 
   try {
+    console.log('Checking environment variables...');
     // Check if all required environment variables are present
     const requiredEnvVars = ['APPLE_ID', 'APPLE_ID_PASSWORD', 'APPLE_TEAM_ID'];
-    for (const envVar of requiredEnvVars) {
-      if (!process.env[envVar]) {
-        throw new Error(`Missing required environment variable: ${envVar}`);
-      }
+    const missingEnvVars = requiredEnvVars.filter((envVar) => !process.env[envVar]);
+
+    if (missingEnvVars.length > 0) {
+      console.warn(
+        `Skipped macOS notarization: 'notarize' options were unable to be generated. Missing environment variables: ${missingEnvVars.join(
+          ', ',
+        )}`,
+      );
+      return;
     }
 
+    console.log('Notarizing...');
     await notarize({
       appBundleId: build.appId,
       appPath: `${appOutDir}/${appName}.app`,
